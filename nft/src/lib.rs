2d2f6916437d1a75841d2cb0b2ad5333a6386eb9
Admin <luxgangzz1@gmail.com>
2022-08-20 19:38:13 +0700

    first commit

--- nft/src/lib.rs
+++ nft/src/lib.rs
@@ -1,7 +1,7 @@
 /*!
 Non-Fungible Token implementation with JSON serialization.
 NOTES:
-  - The maximum balance value is limited by U128 (2**128 - 1).
+  - The maximum balance value is limited by   (2**128 - 1).
   - JSON calls should pass U128 as a base-10 string. E.g. "100".
   - The contract optimizes the inner trie structure by hashing account IDs. It will prevent some
     abuse of deep tries. Shouldn't be an issue, once NEAR clients implement full hashing of keys.
@@ -32,6 +32,8 @@ pub struct Contract {
     tokens: NonFungibleToken,
     metadata: LazyOption<NFTContractMetadata>,
 }
+
+
 
 const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
 
@@ -97,6 +99,10 @@ impl Contract {
     ) -> Token {
         self.tokens.mint(token_id, receiver_id, Some(token_metadata))
     }
+
+
+  
+
 }
 
 near_contract_standards::impl_non_fungible_token_core!(Contract, tokens);
